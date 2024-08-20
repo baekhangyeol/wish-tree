3360215b7fe10fe03bee155ce31d83c1c8afdd9a
hangyeol <snake011128@naver.com>
2024-08-20 09:47:28 +0900

    first commit

---
+++ src/main/java/com/techeer/wishtree/WishTreeApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.wishtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishTreeApplication.class, args);
+    }
+
+}
